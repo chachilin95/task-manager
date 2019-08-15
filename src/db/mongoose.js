0906e31e375c748500764da0aeede332147a747a
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-14 22:34:52 -0500

    Setup POST /user command

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,48 +1,8 @@
 const mongoose = require('mongoose');
-const validator = require('validator');
 
 mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', { 
     useCreateIndex: true,
     useNewUrlParser: true
-});
-
-const User = mongoose.model('User', { 
-    name: {
-        type: String,
-        required: true,
-        trim: true
-    },
-    age: {
-        type: Number,
-        default: 0,
-        validate(value) {
-            if (value < 0) {
-                throw new Error('Age must be a positive number');
-            }
-        }
-    },
-    email: {
-        type: String,
-        required: true,
-        trim: true,
-        lowercase: true,
-        validate(value) {
-            if (!validator.isEmail(value)) {
-                throw new Error('Email is invalid');
-            }
-        }
-    },
-    password: {
-        type: String,
-        required: true,
-        minLength: 7,
-        trim: true,
-        validate(value) {
-            if (value.includes('password')) {
-                throw new Error('Password cannot be "password"');
-            }
-        }
-    }
 });
 
 const Task = mongoose.model('Task', {
--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
+require('./db/mongoose');
 const express = require('express');
+const User = require('./models/user');
 
 const app = express();
 const port = process.env.PORT || 8080;
 
+app.use(express.json());
+
 app.post('/users', (req, res) => {
-    res.send('testing...');
+    const user = new User(req.body);
+
+    user.save().then(() => {
+        res.send(user);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
 });
 
 app.listen(port, () => {
