32a2c6ec89502b4ca7775ee92ccc868df6c7f14e
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-14 22:43:16 -0500

    Created POST /tasks command

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -4,14 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/task-manager-api', {
     useCreateIndex: true,
     useNewUrlParser: true
 });
-
-const Task = mongoose.model('Task', {
-    description: {
-        type: String,
-        required: true
-    },
-    completed: {
-        type: Boolean,
-        default: false
-    }
-});
--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 require('./db/mongoose');
 const express = require('express');
+
+// models
 const User = require('./models/user');
+const Task = require('./models/task');
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -11,7 +14,17 @@ app.post('/users', (req, res) => {
     const user = new User(req.body);
 
     user.save().then(() => {
-        res.send(user);
+        res.status(201).send(user);
+    }).catch((error) => {
+        res.status(400).send(error);
+    });
+});
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body);
+
+    task.save().then(() => {
+        res.status(201).send(task);
     }).catch((error) => {
         res.status(400).send(error);
     });
