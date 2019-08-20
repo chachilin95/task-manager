1e7f7f579416fef5c32abdb5008f337d850c8024
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-20 18:28:01 -0500

    added 'completed' filter to GET /tasks

--- src/routers/task.js
+++ src/routers/task.js
@@ -6,8 +6,18 @@ const auth = require('../middleware/auth');
 
 // get all tasks
 router.get('/tasks', auth, async (req, res) => {
+
+    // gather queries
+    const match = {};
+    if (req.query.completed) {
+        match.completed = req.query.completed === 'true';
+    }
+
     try {
-        await req.user.populate('tasks').execPopulate();
+        await req.user.populate({
+            path: 'tasks',
+            match
+        }).execPopulate();
         res.send(req.user.tasks);
     } catch (error) {
         res.status(500).send(error);
