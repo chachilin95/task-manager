f5ee3196ef91f88d0cc770f9639df397bb69e1bc
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-23 08:40:53 -0500

    Added more robust expectations for user tests

--- tests/routers/user.test.js
+++ tests/routers/user.test.js
@@ -10,22 +10,40 @@ beforeEach(async () => {
 });
 
 test('Should signup a new user', async () => {
-    const { name, email, password } = users[1];
-    const user = { email, password, name };
-
-    await request(app)
+    const { email, password, name } = users[1];
+    
+    // assert successful signup
+    const sendUser = { email, password, name };
+    const response = await request(app)
         .post('/users')
-        .send(user)
+        .send(sendUser)
         .expect(201);
+
+    // assert user was added to the database
+    const createdUser = await User.findById(response.body.user._id);
+    expect(createdUser).not.toBeNull();
+
+    // assert content of created user
+    expect(response.body).toMatchObject({
+        user: { email, name },
+        token: createdUser.tokens[0].token
+    });
+    expect(createdUser.password).not.toBe(password);
 });
 
 test('Should login an existing user', async () => {
-    const user = users[0];
-
-    await request(app)
+    const { _id, email, password } = users[0];
+    
+    // assert successful login
+    const loginUser = { email, password };
+    const response = await request(app)
         .post('/users/login')
-        .send(user)
+        .send(loginUser)
         .expect(200);
+
+    // assert auth tokens match
+    const user = await User.findById(_id);
+    expect(response.body.token).toBe(user.tokens[1].token);
 });
 
 test('Should not login non-existent user', async () => {
@@ -56,14 +74,19 @@ test('Should not get profile for unauthenticated user', async () => {
 });
 
 test('Should delete account for user', async () => {
-    const user = users[0];
-    const token = user.tokens[0].token;
+    const { _id, tokens } = users[0];
+    const token = tokens[0].token;
 
+    // assert successful delete
     await request(app)
         .delete('/users/me')
         .set('Authorization', `Bearer ${token}`)
         .send()
         .expect(200);
+
+    // assert user was deleted from database
+    const user = await User.findById(_id);
+    expect(user).toBeNull();
 });
 
 test('Should not delete account for unauthenticated user', async () => {
