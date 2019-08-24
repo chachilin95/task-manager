fe56f5a91d677c6696cc4ad72164973fddffe04b
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-24 10:01:59 -0500

    Fixed sendgrid mock and renamed mocks dir correctly

---
+++ tests/__mocks__/@sendgrid/mail.js
@@ -0,0 +1,8 @@
+module.exports = {
+    setApiKey() {
+        // do nothing
+    },
+    send() {
+        // do nothing
+    }
+}
--- tests/mocks/@sendgrid/mail.js
+++
@@ -1,7 +0,0 @@
-const setApiKey = (key) => key;
-const send = (body) => body;
-
-module.exports = {
-    setApiKey,
-    send
-}
