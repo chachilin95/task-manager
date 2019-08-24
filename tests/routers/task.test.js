899bd40a388212a5facd8ccc8b74f402aa97ea66
Alejandro Figueroa <droppingbasses@Alejandros-MacBook-Pro.local>
2019-08-24 10:38:39 -0500

    Added tests for getting and deleting tasks

--- tests/routers/task.test.js
+++ tests/routers/task.test.js
@@ -4,7 +4,6 @@ const app = require('../../src/app');
 const { setupDatabase } = require('../fixtures/db');
 
 // models
-const User = require('../../src/models/user');
 const Task = require('../../src/models/task');
 
 // fixtures
@@ -33,4 +32,44 @@ test('Should create task for user', async () => {
 
     // assert content of added task
     expect(task.completed).toEqual(false);
+});
+
+test('Should get tasks for user', async () => {
+    const { _id, tokens } = users[0];
+    const token = tokens[0].token;
+
+    // get number of tasks the user owns
+    let numTasksExpected = 0;
+    tasks.forEach((task) => {
+        if (task.owner === _id) {
+            numTasksExpected++;
+        }
+    });
+
+    // assert successful retrieval
+    const response = await request(app)
+        .get('/tasks')
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(200);
+
+    // assert number of tasks returned
+    expect(response.body.length).toEqual(numTasksExpected);
+});
+
+test('Should not delete unowned task', async () => {
+    const { tokens } = users[0];
+    const { _id } = tasks[1]; // users[1] owns tasks[1]
+    const token = tokens[0].token;
+
+    // assert rejected deletion
+    await request(app)
+        .delete(`/tasks/${_id}`)
+        .set('Authorization', `Bearer ${token}`)
+        .send()
+        .expect(404);
+
+    // assert task was not deleted
+    const task = await Task.findById(_id);
+    expect(task).not.toBeNull();
 });
